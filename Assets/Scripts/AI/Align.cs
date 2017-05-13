e69f34ed0f7bc3a0518317dc70240aeb574e5375
SupremeRulerJiggles <jolttwolff@gmail.com>
2017-05-13 12:45:51 -0500

    Working on the signed angle method

--- Assets/Scripts/AI/Align.cs
+++ Assets/Scripts/AI/Align.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class Align : MonoBehaviour 
 {
+	GameObject character;
+	[SerializeField] GameObject target;
 
+	[SerializeField] float maxAngularAcceleration;
+	[SerializeField] float maxRotation;
+
+	[SerializeField] float targetRadius;
+	[SerializeField] float slowRadius;
+
+	[SerializeField] float timeToTarget = .1f;
+
+	public float modNum = 50;
+
+	void Start () 
+	{
+		character = gameObject;
+		if(!target)
+			target = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update()
+	{
+		print(Math.MapToRangePI(-655));
+		print(Math.MapToRange(Math.MapToRangePI(-655), -Mathf.PI, Mathf.PI, 0, 10));
+	}
+
+	SteeringOutput GetAcceleration()
+	{
+		SteeringOutput acceleration = new SteeringOutput();
+
+		Vector3 targetOrientation = target.transform.forward;
+		Vector3 characterOrientation = character.transform.forward;
+
+		Vector3 rotation = targetOrientation - characterOrientation;
+
+		return null;
+	}
 }
--- Assets/Scripts/Utility/Math.cs
+++ Assets/Scripts/Utility/Math.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public static class Math
 {
-	public static float randomBinomial()
+	public static float RandomBinomial()
 	{
 		return Random.Range(0f, 1f) - Random.Range(0f, 1f);
+	}
+
+	public static float MapToRange(float num, float oldMin, float oldMax, float newMin, float newMax)
+	{
+		return (num - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+	}
+
+	// Maps a given float to a number between (-Pi, Pi]
+	// Assumes that the number given is based in the range from [0, 360)
+	public static float MapToRangePI(float num)
+	{
+		float mod = num % 360;
+		float multiple = (num - mod) / 360;
+		float sign = num / Mathf.Abs(num);
+
+		if(Mathf.Abs(mod) > 180)
+		{
+			return Math.MapToRange(
+				(sign * Mathf.Abs(num)) - (multiple * 360), 
+				sign * 180, sign * 360, 
+				sign * -Mathf.PI, 0
+			);
+		}
+		else
+		{
+			return Math.MapToRange(num, 0, 180, 0, Mathf.PI) - (2*multiple*Mathf.PI);
+		}
 	}
 }
